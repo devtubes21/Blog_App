d8b83ad14073eba34240c225ff87f088c59de27a
Udara <udara@gmail.com>
2021-07-06 22:22:58 +0530

    created new things

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,3 +1,29 @@
+<style>
+
+  #create{
+    border: 1px solid #0066ff;
+    color: #0066ff;
+  }
+
+  #create:hover{
+    border: 1px solid #0066ff;
+    color: #fff;
+    background-color: #0066ff;
+  }
+
+  #sign_out{
+    border: 1px solid #e60000;
+    color: #e60000;
+  }
+
+  #sign_out:hover{
+    border: 1px solid #e60000;
+    color: #fff;
+    background-color: #e60000;
+  }
+
+</style>
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" href="index.php">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -29,14 +55,41 @@
     </ul>
     
     <ul class="navbar-nav">
-    
-      <li class="nav-item">
-        <a class="nav-link" href="sign_in.php">Sign In</a>
-      </li>
 
-      <li class="nav-item">
-        <a class="nav-link" href="sign_up.php">Sign Up</a>
-      </li>
+      <?php
+      
+        if(isset($_SESSION['User_Fname'])){
+          
+          echo "
+          
+                <li class='nav-item mr-2'>
+                <a id='create' class='nav-link' href='create_post.php'>Create</a>
+                </li>
+
+                <li class='nav-item'>
+                <a id='sign_out' class='nav-link' href='sign_out.php'>Sign Out</a>
+                </li>
+
+          ";
+
+        }
+        else{
+
+          echo "
+          
+          <li class='nav-item'>
+          <a class='nav-link' href='sign_in.php'>Sign In</a>
+          </li>
+
+          <li class='nav-item'>
+          <a class='nav-link' href='sing_up.php'>Sign Up</a>
+          </li>
+
+    ";
+
+        }
+      
+      ?>
 
     </ul>
 
